293a92ad265d270b65a844b36b7a6d083148dac4
nophDog <zjx970912@163.com>
2020-01-28 15:43:56 +0800

    OpenSSH setup removed

--- serverSetup.ps1
+++ serverSetup.ps1
@@ -19,8 +19,3 @@ ForEach-Object { Invoke-Expression -Command "scoop bucket add $_" -WarningAction
 ## usefule modules
 @('scoop-completion','PoshGram','PSShareGoods','PSScriptTools') | 
 ForEach-Object { Install-Module -Name $_ -Scope CurrentUser -Force -ErrorAction Ignore }
-
-## install sshd service
-$file = "$env:USERPROFILE\$fileName"
-Invoke-WebRequest -Uri 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip' -OutFile $file
-Expand-Archive -Path 
