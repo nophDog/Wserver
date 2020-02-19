29d3fae5f783325e017866a6acca6ec80819a9ea
nophDog <zjx970912@163.com>
2020-02-20 07:51:37 +0800

    Install script added

--- serverSetup.ps1
+++ serverSetup.ps1
@@ -8,14 +8,15 @@ echo '                          /____/        /_/              '
 
 
 [System.Environment]::SetEnvironmentVariable('SCOOP', "$env:USERPROFILE\scoop", [System.EnvironmentVariableTarget]::User)
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
 ## scoop third-party buckets
 scoop bucket add 'scoop-completion' 'https://github.com/Moeologist/scoop-completion'
 scoop bucket add 'random' 'https://github.com/nophDog/random'
 
-@('main','extras','versions','nirsoft','nerd-fonts','nonportable','jetbrains') | 
+@('main','extras','versions','nirsoft','nerd-fonts','nonportable','jetbrains') |
 ForEach-Object { Invoke-Expression -Command "scoop bucket add $_" -WarningAction Ignore -ErrorAction Ignore }
 
 ## usefule modules
-@('scoop-completion','PoshGram','PSShareGoods','PSScriptTools') | 
+@('scoop-completion','PoshGram','PSShareGoods','PSScriptTools') |
 ForEach-Object { Install-Module -Name $_ -Scope CurrentUser -Force -ErrorAction Ignore }
