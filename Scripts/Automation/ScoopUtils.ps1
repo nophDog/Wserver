f21ce222ace0fbedb0abdb4f171e4ea34cedf11b
nophDog <zjx970912@163.com>
2020-03-21 17:27:14 +0800

    ScoopUtils: sii fix

--- Scripts/Automation/ScoopUtils.ps1
+++ Scripts/Automation/ScoopUtils.ps1
@@ -235,7 +235,7 @@ function sii {
         # https://ridicurious.com/2018/10/17/4-ways-to-find-os-architecture-using-powershell-32-or-64-bit/
     )
 
-    scoop install $Package
+    scoop install $Package --arch $Arch
 
     Update-NotInstalled
     Update-Installed
